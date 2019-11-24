d4bd5c87fb959f69aa5daea463aaf6f2d1108da4
pawel <pawel.niechoda@gmail.com>
2019-11-24 08:13:10 +0200

    update port number

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const io = require('socket.io')(server);
 const { getRandomString } = require('./utils');
 const { getWaterQualityFeatures } = require('./features/waterQuality');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 server.listen(port, () => console.log(`Listening on ${port}`));
 
